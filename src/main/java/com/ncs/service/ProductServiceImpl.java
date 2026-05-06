a664a86ef39b2d0c56d2f659e3a8bf92c3c9791b
SagarKanungo8 <sagar.kanungo08@gmail.com>
2026-05-06 11:37:20 +0530

    productservice updated

--- src/main/java/com/ncs/service/ProductServiceImpl.java
+++ src/main/java/com/ncs/service/ProductServiceImpl.java
@@ -99,7 +99,7 @@ public class ProductServiceImpl implements ProductService{
 		
 		productRepo.delete(product);
 
-		return "Product Deleted Successfullyyyyy...!!!";
+		return "Product is now Deleted Successfullyyyyy...!!!";
 	}
 
 	@Override
